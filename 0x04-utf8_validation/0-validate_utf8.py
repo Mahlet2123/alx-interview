9a6d2ce9cb1a9133733da0ec9e5aca553ed54091
mahlet <akotet2123@gmail.com>
2023-08-27 01:22:06 +0000

    some fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,28 +7,22 @@ def validUTF8(data):
     a method that determines if a given data set
     represents a valid UTF-8 encoding.
     """
-    num_bytes_to_follow = 0
+    num_following_bytes= 0
 
     for byte in data:
-        if byte in range(0, 256):
-            continue
+        if num_following_bytes > 0:
+            if (byte >> 6) != 0b10:
+                return False
+            num_following_bytes -= 1
+        elif (byte >> 7) == 0b0:
+            num_following_bytes = 0
+        elif (byte >> 5) == 0b110:
+            num_following_bytes = 1
+        elif (byte >> 4) == 0b1110:
+            num_following_bytes = 2
+        elif (byte >> 3) == 0b11110:
+            num_following_bytes = 3
         else:
             return False
 
-    if num_bytes_to_follow == 0:
-            if (byte >> 7) == 0b0:
-                continue
-            elif (byte >> 5) == 0b110:
-                num_bytes_to_follow = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes_to_follow = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes_to_follow = 3
-            else:
-                return False
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            num_bytes_to_follow -= 1
-
-    return num_bytes_to_follow == 0
+    return num_following_bytes == 0
